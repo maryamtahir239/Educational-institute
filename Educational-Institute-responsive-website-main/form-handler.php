e1c2015b77bbd30be78765f04f2631913a3a5621
maryamtahir239 <107819644+maryamtahir239@users.noreply.github.com>
2024-07-21 15:42:30 +0500

    Add files via upload

---
+++ Educational-Institute-responsive-website-main/form-handler.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$visitor-email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+$email_subject = 'New Form Submission';
+$email_body= "User Name: $name.\n".
+            "User Email: $visitor-email.\n".
+            "Subject: $subject .\n".
+            "User Message: $message.\n";
+            $to = ' avinash6252@gmail.com';
+            $headers = "From: $email_from \r\n";
+            $headers .="Reply-To: $visitor-email \r\n";
+            mail($to,$email_subject ,$email_body,$headers);
+            header("Location: contact.html");
+
+?>
